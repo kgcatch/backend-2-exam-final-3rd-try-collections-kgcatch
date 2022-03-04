ea16c889248f7adb3e22b59e57d168e0a55dff37
kgcatch <kecskesgabi@gmail.com>
2022-03-04 15:53:45 +0100

    First commit 0/6

---
+++ src/main/java/com/codecool/science/Article.java
@@ -0,0 +1,21 @@
+package com.codecool.science;
+
+import javax.swing.border.TitledBorder;
+
+public class Article {
+    public Article(String author, String title, int year) {
+    }
+
+    public static <R> R getYear(Article article) {
+        return null;
+    }
+
+    public static <R> R getAuthor(Article article) {
+            return null;
+    }
+
+
+    public static Object getTitle(Article article) {
+        return null;
+    }
+}
---
+++ src/main/java/com/codecool/science/ScienceJournal.java
@@ -0,0 +1,25 @@
+package com.codecool.science;
+
+import java.util.List;
+
+public class ScienceJournal {
+    public void add(Article article) {
+
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public List<Article> findByAuthor(String author) {
+        return null;
+    }
+
+    public List<Article> findByAuthorAndYear(String author, int year) {
+        return null;
+    }
+
+    public List<Article> findByAuthorLike(String article) {
+        return null;
+    }
+}
